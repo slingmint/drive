2cd20881463bc411c5e9365b9afd4f27f03a7688
Me <slingmint@gmail.com>
2021-03-03 10:34:42 -0600

    edited

--- configure-panels.sh
+++ configure-panels.sh
@@ -1,6 +1,16 @@
 #https://github.com/mate-desktop/marco
 
 gsettings set org.mate.Marco.general num-workspaces 5
+gsettings set org.mate.Marco.workspace-names name-1 MAIN
+gsettings set org.mate.Marco.workspace-names name-2 WEB
+gsettings set org.mate.Marco.workspace-names name-3 ADSTAB
+gsettings set org.mate.Marco.workspace-names name-4 DPDEV
+gsettings set org.mate.Marco.workspace-names name-5 OPEN
+
+
+
+
 
 #gsettings set org.mate.Marco.global-keybindings switch-to-workspace-1 '<Alt>F1'
+
 
