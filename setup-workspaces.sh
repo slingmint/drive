fe021f98361ec4c03ff2fcd2e6a3eafe56378fd8
me <slingmint@gmail.com>
2021-03-05 16:42:12 -0600

    done

---
+++ setup-workspaces.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download Applets
+
+cd ~
+
+sudo wget https://cinnamon-spices.linuxmint.com/files/applets/workspace-grid@hernejj.zip
+
+sudo unzip ~/workspace-grid@hernejj.zip -d ~/.local/share/cinnamon/applets/
+
+
