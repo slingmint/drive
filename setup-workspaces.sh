571841c6af44f3cd28d48c934750ec34313c94bd
me <slingmint@gmail.com>
2021-03-07 07:55:31 -0600

    use the hernejj version of the workspace switcher utility

--- setup-workspaces.sh
+++ setup-workspaces.sh
@@ -2,10 +2,19 @@
 
 # Download Applets
 
-cd ~
 
-sudo wget https://cinnamon-spices.linuxmint.com/files/applets/workspace-grid@hernejj.zip
 
-sudo unzip ~/workspace-grid@hernejj.zip -d ~/.local/share/cinnamon/applets/
+sudo cat ~/.local/share/cinnamon/applets/workspace-grid@hernejj/settings-schema.json | jq '.numCols.value = 3'  | jq '.numRows.value = 3' > ~/.cinnamon/configs/workspace-grid@hernejj/workspace-grid@hernejj.json
 
+
+#cd ~
+
+#sudo wget https://cinnamon-spices.linuxmint.com/files/applets/workspace-grid@hernejj.zip
+
+#sudo unzip ~/workspace-grid@hernejj.zip -d ~/.local/share/cinnamon/applets/
+
+
+#sudo apt-get install jq -y
+
+#sudo cat ~/.cinnamon/configs/workspace-grid@hernejj/workspace-grid@hernejj.json | jq '.numCols.value = 2, .numRows.value=4' > ~/.cinnamon/configs/workspace-grid@hernjj/workspace3-grid@hernjj.json
 
