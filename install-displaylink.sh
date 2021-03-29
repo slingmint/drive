949f67e453ee455a2725c47cb8a7018753734fe7
root <slingmint@gmail.com>
2021-03-29 17:11:04 +0000

    added displaylink drivers for monitor setup

---
+++ install-displaylink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#mkdir ~/tmp
+
+#unzip displaylink5.3.1.zip -d ~/tmp
+
+sudo chmod +x ~/tmp/displaylink-driver-5.3.1.34.run
+
+sudo apt-get install libc6-dev -y
+
+sudo ~/tmp/displaylink-driver-5.3.1.34.run
+
+
